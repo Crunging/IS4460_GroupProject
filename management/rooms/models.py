7c30adbaadb856abd0b40116c03e9da7ec216763
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-03-31 14:00:10 -0600

    Update models.py

--- management/rooms/models.py
+++ management/rooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Room(models.model):
+    RoomID = models.AutoField
+    BuildingID =models.ForeignKey(Building, on_delete=models.Cascade)
+    Room_Number =models.IntegerField()
+    Type = models.CharField(max_length=255)
 
 # Create your models here.
