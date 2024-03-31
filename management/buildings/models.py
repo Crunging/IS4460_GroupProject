3bceae218afb3cffc0edf3b08df8b3928996407e
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-03-31 13:54:07 -0600

    Update models.py

--- management/buildings/models.py
+++ management/buildings/models.py
@@ -3,6 +3,12 @@ class Building(models.Model):
     BuildingID = models.AutoField
     Building_Name = models.CharField(max_length=150)
     Year_Built = models.DateField(max_length=15)
+
+class Room(models.model):
+    RoomID = models.AutoField
+    BuildingID =models.ForeignKey(Building, on_delete=models.Cascade)
+    Room_Number =models.IntegerField()
+    Type = models.CharField(max_length=255)
 # Create your models here.
 
 
