0f40df6ef70d49e33fe7998aca8871e81005d4d9
Arjun Pillai <arjunpillai35@gmail.com>
2024-03-31 14:13:37 -0600

    Persons View Commit

---
+++ management/persons/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+from .models import Person
+from .forms import PersonForm
+
+class PersonList(View):
+    def get(self, request):
+        persons = Person.objects.all()
+        return render(request=request, template_name='persons/person_list.html', context={'persons': persons})
+
+class PersonDetail(View):
+    def get(self, request, uid):
+        person = Person.objects.get(uid=uid)
+        return render(request=request, template_name='persons/person_detail.html', context={'person': person})
+
+class PersonCreate(View):
+    def get(self, request):
+        form = PersonForm()
+        return render(request=request, template_name='persons/person_form.html', context={'form': form})
+
+    def post(self, request):
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('person_list'))
+        return render(request=request, template_name='persons/person_form.html', context={'form': form})
+
+class PersonUpdate(View):
+    def get(self, request, uid):
+        person = Person.objects.get(uid=uid)
+        form = PersonForm(instance=person)
+        return render(request=request, template_name='persons/person_form.html', context={'form': form})
+
+    def post(self, request, uid):
+        person = Person.objects.get(uid=uid)
+        form = PersonForm(request.POST, instance=person)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('person_list'))
+        return render(request=request, template_name='persons/person_form.html', context={'form': form})
+
+class PersonDelete(View):
+    def get(self, request, uid):
+        person = Person.objects.get(uid=uid)
+        form = PersonForm(instance=person)
+        return render(request=request, template_name='persons/person_delete.html', context={'person': person, 'form': form})
+
+    def post(self, request, uid):
+        person = Person.objects.get(uid=uid)
+        person.delete()
+        return redirect(reverse('person_list'))
