ba34e7f1877fb09f3e77182c489adc186e326010
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-03-31 13:50:06 -0600

    Update models.py

--- management/buildings/models.py
+++ management/buildings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+class Building(models.Model):
+    BuildingID = models.AutoField
+    Building_Name = models.CharField(max_length=150)
+    Year_Built = models.DateField(max_length=15)
 # Create your models here.
+
+
