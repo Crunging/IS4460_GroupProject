e73efc54eec12dc74a7797fb735aef1b2e012c25
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-03-31 14:17:00 -0600

    Update views.py

--- management/buildings/views.py
+++ management/buildings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from buildings.models import buildings
+from buildings.models import building
 from django.views import View
 from .forms import BuildingForm
 from rest_framework import generics
