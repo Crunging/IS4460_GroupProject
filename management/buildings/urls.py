c1cf02a9fa00a3273f5bc568fd05a80aeb561fd5
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-04-08 14:47:45 -0600

    Update urls.py

--- management/buildings/urls.py
+++ management/buildings/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from .views import BuildingList, BuildingDetail, BuildingCreate, BuildingUpdate, BuildingDelete
 
+urlpatterns = [
+    path('', PersonList.as_view(), name='person_list'),
+    path('<int:uid>/', PersonDetail.as_view(), name='person_detail'),
+    path('create/', PersonCreate.as_view(), name='person_create'),
+    path('<int:uid>/update/', PersonUpdate.as_view(), name='person_update'),
+    path('<int:uid>/delete/', PersonDelete.as_view(), name='person_delete'),
+]
