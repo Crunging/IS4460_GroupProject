0e16016e5bf4a350776d94c00a0ecb8e2e75547c
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-04-11 20:31:37 -0600

    Update views.py

--- management/access_permissions/views.py
+++ management/access_permissions/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import AccessPermissions
+from .forms import AccessPermissionsForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+# List view for all access records
+class AccessPermissionListView(ListView):
+    model = AccessPermission
+    context_object_name = 'access_permissions'
+    template_name = 'access_permissions/accesspermissions_list.html'
+
+# Detail view for a specific access record
+class AccessPermissionDetailView(DetailView):
+    model = AccessPermission
+    context_object_name = 'access_permission'
+    template_name = 'access_permission/accesspermission_detail.html'
+
+# Create view for a new access record
+class AccessPwermissionCreateView(CreateView):
+    model = AccessPermission
+    form_class = AccessPermissionForm
+    template_name = 'access_permission/accesspermission_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('access_permission_detail', kwargs={'pk': self.object.pk})
+
+# Update view for an existing access record
+class AccessPermissionUpdateView(UpdateView):
+    model = AccessPermission
+    form_class = AccessPermissionForm
+    template_name = 'access_permissions/accesspermission_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('access_permission_detail', kwargs={'pk': self.object.pk})
+
+# Delete view for an access record
+class AccessPermissionDeleteView(DeleteView):
+    model = AccessPermission
+    context_object_name = 'access_permission'
+    template_name = 'access_permissions/accesspermission_confirm_delete.html'
+    success_url = reverse_lazy('access_permission_list')
