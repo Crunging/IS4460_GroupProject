007f4e00e44b3fb60f1f4bfc1290e5b17b96118d
marielisalv <156021731+marielisalv@users.noreply.github.com>
2024-04-11 20:51:11 -0600

    Create urls.py

---
+++ management/rooms/urls.py
@@ -0,0 +1,9 @@
+from django.db import models
+from .models import Building 
+
+class Room(models.Model):  
+    RoomID = models.AutoField(primary_key=True) 
+    BuildingID = models.ForeignKey(Building, on_delete=models.CASCADE) 
+    Room_Number = models.IntegerField()
+    Type = models.CharField(max_length=255)
+
