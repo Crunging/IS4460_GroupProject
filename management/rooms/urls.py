7321b78c2005c06d8c5bb387c764288627cdba9a
marielisalv <156021731+marielisalv@users.noreply.github.com>
2024-04-11 21:35:24 -0600

    Update urls.py

--- management/rooms/urls.py
+++ management/rooms/urls.py
@@ -1,9 +1,11 @@
-from django.db import models
-from .models import Building 
+from django.urls import path
+from .views import RoomsView, RoomsUpdate, RoomsDelete, RoomsAdd
 
-class Room(models.Model):  
-    RoomID = models.AutoField(primary_key=True) 
-    BuildingID = models.ForeignKey(Building, on_delete=models.CASCADE) 
-    Room_Number = models.IntegerField()
-    Type = models.CharField(max_length=255)
+urlpatterns = [
+    path('', RoomsView.as_view(), name='rooms_view'),
+    path('<int:room_id>/', RoomsUpdate.as_view(), name='room_update'),  # Changed name to 'room_update'
+    path('<int:room_id>/update/', RoomsUpdate.as_view(), name='room_update'),  # Changed name to 'room_update'
+    path('<int:room_id>/delete/', RoomsDelete.as_view(), name='room_delete'),  # Changed name to 'room_delete'
+    path('add/', RoomsAdd.as_view(), name='room_add'),  # Changed URL pattern to remove 'room_id'
+]
 
