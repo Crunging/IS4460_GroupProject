1e4350d122ec53d8e94abedd99ec047967c730fe
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-04-11 22:44:27 -0600

    Update views.py

--- management/access_records/views.py
+++ management/access_records/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
-from .models import Access_Recordss
+from .models import Access_Records
 from .forms import AccessRecordsForm
 
 class AccessRecordsList(View):
     def get(self, request):
-        access_records = AccessRecord.objects.all()
+        access_records = Access_Records.objects.all()
         return render(request=request, template_name='access_records/accesrecords_list.html', context={'access_Records': Access_Recordss})
     
 class AccessRecordsDetail(View):
     def get(self, request, RecordID):
-        Access_Records = accessrecord.objects.get(RecordID=RecordID)
+        Access_Records = Access_Records.objects.get(RecordID=RecordID)
         return render(request=request, template_name='access_records/accessRecords_detail.html', context={'access_record': Access_Recordss})
 
 class AccessRecordsUpdate(View):
     def get(self, request, RecordID=None):
         if RecordID:
-            access_record = AccessRecord.objects.get(pk=RecordID)
+            access_record = Access_Records.objects.get(pk=RecordID)
         else:
             access_record = Access_Records()
         form = AccessRecordsForm(instance=access_record)
@@ -28,7 +28,7 @@ class AccessRecordsUpdate(View):
             access_record = Access_Records.objects.get(pk=RecordID)
         else:
             access_record = Access_Records()
-        form = AccesRecord_Form(request.POST, instance=Access_Records)
+        form = AccesRecord_Form(request.POST, instance=access_record)
         if form.is_valid():
             form.save()
             return redirect(reverse("accessrecord_list"))
@@ -37,10 +37,10 @@ class AccessRecordsUpdate(View):
 class AccessRecordsDelete(View):
     def get(self, request, RecordID=None):
         if RecordID:
-            access_record = Rccess_Record.objects.get(pk=RecordID)
+            access_record = Access_Records.objects.get(pk=RecordID)
         else:
             access_record = Access_Records()
-        form = AccessRecordsForm(instance=Access_Records)
+        form = AccessRecordsForm(instance=access_record)
         for field in form.fields:
             form.fields[field].widget.attrs['disabled'] = True
         return render(request=request, template_name='access_records/accessrecord_delete.html', context={'access_record': Access_Records, 'form': form})
@@ -52,11 +52,11 @@ class AccessRecordsDelete(View):
 
 class AccessRecordsAdd(View):
     def get(self, request):
-        form = accessrecordForm()
-        return render(request, 'Access_Records/accessrecord_create.html', {'form': form})
+        form = AccessRecordForm()
+        return render(request, 'access_records/accessrecord_create.html', {'form': form})
 
     def post(self, request):
-        form = accessrecordForm(request.POST)
+        form = AccessRecordForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect(reverse('accessrecord_list'))
