a971cae112636bb49b8789c38a9bf90e3610f0b2
marielisalv <156021731+marielisalv@users.noreply.github.com>
2024-04-11 20:45:58 -0600

    Update views.py

--- management/rooms/views.py
+++ management/rooms/views.py
@@ -1,91 +1,58 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
-from rooms.models import Rooms
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Room  # Correct model import
 from django.views import View
 from .forms import RoomsForm
-from rest_framework import generics
-
 
 class RoomsView(View):
+    def get(self, request):
+        rooms = Room.objects.all()
+        return render(request, 'rooms_view.html', {'rooms': rooms})  # Corrected context dictionary key
 
-    def get(self,request):
-
-        rooms = Rooms.objects.all()
-
-        return render(request = request,template_name = 'rooms_view.html',context = {'rooms:rooms'})
-
-                                                                                    
 class RoomsUpdate(View):
-
-    def get(self,request,RoomID=None):
-
+    def get(self, request, RoomID=None):
         if RoomID:
-            room = Rooms.objects.get(pk=RoomID)
+            room = Room.objects.get(pk=RoomID)
         else:
-            room = Rooms()
+            room = Room()
+        form = RoomsForm(instance=room)
+        return render(request, 'rooms_update.html', {'room': room, 'form': form})
 
-        form = RoomForm(instance=room)
-
-
-       
-        return render(request = request,
-                      template_name = 'rooms_update.html',
-                      context = {'room':room,'form':form})
-    
-    def post(self,request,RoomID=None):
-
+    def post(self, request, RoomID=None):
         if RoomID:
-            room = Rooms.objects.get(pk=RoomID)
+            room = Room.objects.get(pk=RoomID)
         else:
-            room = room()
-
-        form = RoomForm(request.POST,instance=room)
-
+            room = Room()
+        form = RoomsForm(request.POST, instance=room)
         if form.is_valid():
-            room = form.save()
-
+            form.save()
             return redirect(reverse("rooms_update"))
-        
-        return render(request = request,template_name = 'rooms_update.html',context = {'room':room,'form':form})
-     
-class RoomDelete(View):
+        return render(request, 'rooms_update.html', {'room': room, 'form': form})
 
-    def get(self,request,RoomID=None):
-
+class RoomsDelete(View):
+    def get(self, request, RoomID=None):
         if RoomID:
-            room = Rooms.objects.get(pk=RoomID)
+            room = Room.objects.get(pk=RoomID)
         else:
-            room = room()
-
-      
-        form = RoomForm(instance=room)
-
+            room = Room()
+        form = RoomsForm(instance=room)
         for field in form.fields:
             form.fields[field].widget.attrs['disabled'] = True
+        return render(request, 'rooms_delete.html', {'room': room, 'form': form})
 
-        return render(request = request,template_name = 'rooms_delete.html',context = {'room':room,'form':form})
-      
-    
-    def post(self,request,RoomID=None):
-
-        room = Rooms.objects.get(pk=RoomID)
-
-        room = RoomForm(request.POST,instance=room)
-
+    def post(self, request, RoomID=None):
+        room = Room.objects.get(pk=RoomID)
         room.delete()
-
         return redirect(reverse("rooms_view"))
 
-class RoomAdd(View):
+class RoomsAdd(View):
     def get(self, request):
-        form = RoomForm()
+        form = RoomsForm()
         return render(request, 'rooms_add.html', {'form': form})
 
     def post(self, request):
-        form = RoomForm(request.POST)
+        form = RoomsForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('rooms_view')
         return render(request, 'rooms_add.html', {'form': form})
-    
-
-# Create your views here.
