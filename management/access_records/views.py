8ef88b6ad5e5440364a857ff60baf0e1f67dc2cc
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-04-11 22:19:51 -0600

    Update views.py

--- management/access_records/views.py
+++ management/access_records/views.py
@@ -1,11 +1,3 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import AccessRecord
-from .forms import AccessRecordForm
-from django.urls import reverse_lazy
-
-
-
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
