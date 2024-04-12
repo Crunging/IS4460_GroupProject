98f50e31d9760d493b3e04f2f429a87c97beda92
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-04-11 22:27:06 -0600

    Update views.py

--- management/access_records/views.py
+++ management/access_records/views.py
@@ -7,60 +7,60 @@ from .forms import BuildingForm
 class AccessRecordsList(View):
     def get(self, request):
         access_records = AccessRecord.objects.all()
-        return render(request=request, template_name='Action_Records/ActionRecords_list.html', context={'Action_Records': Action_Records})
+        return render(request=request, template_name='Access_Records/ActionRecords_list.html', context={'Access_Records': Access_Records})
     
 class accessrecords(View):
     def get(self, request, RecordID):
-        Action_Record = accessrecord.objects.get(RecordID=RecordID)
-        return render(request=request, template_name='Action_Records/ActionRecords_detail.html', context={'Action_Record': Action_Record)
+        Access_Record = accessrecord.objects.get(RecordID=RecordID)
+        return render(request=request, template_name='Access_Records/ActionRecords_detail.html', context={'Access_Record': Access_Record)
 
 class ActionRecordsUpdate(View):
     def get(self, request, RecordID=None):
         if RecordID:
-            Action_Record = accessrecord.objects.get(pk=RecordID)
+            Access_Record = accessrecord.objects.get(pk=RecordID)
         else:
-            Action_Record = Action_Record()
-        form = accessrecordForm(instance=Action_Record)
-        return render(request=request, template_name='Action_Records/accessrecords_update.html', context={'Action_Records': Action_Records, 'form': form})
+            Access_Record = Access_Record()
+        form = accessrecordForm(instance=Access_Record)
+        return render(request=request, template_name='Access_Records/accessrecords_update.html', context={'Access_Records': Access_Records, 'form': form})
     
     def post(self, request, RecordID=None):
         if RecordID:
-            Action_Record = Action_Record.objects.get(pk=RecordID)
+            Access_Record = Access_Record.objects.get(pk=RecordID)
         else:
-            Action_Record = Action_Record()
-        form = Acces_Form(request.POST, instance=Action_Record)
+            Access_Record = Access_Record()
+        form = Acces_Form(request.POST, instance=Access_Record)
         if form.is_valid():
             form.save()
             return redirect(reverse("accessrecord_list"))
-        return render(request=request, template_name='Action_Records/accessrecord_update.html', context={'Action_Record': Action_Record, 'form': form})
+        return render(request=request, template_name='Access_Records/accessrecord_update.html', context={'Access_Record': Access_Record, 'form': form})
 
 class accessrecordDelete(View):
     def get(self, request, RecordID=None):
         if RecordID:
-            Action_Record = Action_Record.objects.get(pk=RecordID)
+            Access_Record = Access_Record.objects.get(pk=RecordID)
         else:
-            Action_Record = Action_Record()
-        form = accessrecordForm(instance=Action_Record)
+            Access_Record = Access_Record()
+        form = accessrecordForm(instance=Access_Record)
         for field in form.fields:
             form.fields[field].widget.attrs['disabled'] = True
-        return render(request=request, template_name='Action_Records/accessrecord_delete.html', context={'Action_Record': Action_Record, 'form': form})
+        return render(request=request, template_name='Access_Records/accessrecord_delete.html', context={'Access_Record': Access_Record, 'form': form})
       
     def post(self, request, RecordID=None):
-        Action_Record = Action_Record.objects.get(pk=RecordID)
-        Action_Record.delete()
+        Access_Record = Access_Record.objects.get(pk=RecordID)
+        Access_Record.delete()
         return redirect(reverse("accessrecord_list"))
 
 class accessrecordAdd(View):
     def get(self, request):
         form = accessrecordForm()
-        return render(request, 'Action_Record/accessrecord_create.html', {'form': form})
+        return render(request, 'Access_Record/accessrecord_create.html', {'form': form})
 
     def post(self, request):
         form = accessrecordForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect(reverse('accessrecord_list'))
-        return render(request, 'Action_Records/accessrecord_create.html', {'form': form})
+        return render(request, 'Access_Records/accessrecord_create.html', {'form': form})
 
 
 
