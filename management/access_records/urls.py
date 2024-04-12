91e4c6aeb06d6adc38cef1eb4a9a6fe74e610c44
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-04-11 23:15:54 -0600

    Update urls.py

--- management/access_records/urls.py
+++ management/access_records/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
-from .views import AccessRecordsListView, AccessRecordsDetailView, AccessRecordsCreateView, AccessRecordsUpdateView, AccessRecordsDeleteView
+from django.urls import path
+from .views import (
+    AccessRecordsList,
+    AccessRecordsDetail,
+    AccessRecordsCreate,
+    AccessRecordsUpdate,
+    AccessRecordsDelete,
+)
 
 urlpatterns = [
-    # URL pattern for the list view of access permissions
-    path('', AccessRecordsListView.as_view(), name='accessrecord_list'),
-    
-    # URL pattern for the detail view of a specific access permission
-    path('<int:pk>/', AccessRecordsDetailView.as_view(), name='accessrecord_detail'),
-    
-    # URL pattern for creating a new access permission
-    path('create/', AccessRecordsCreateView.as_view(), name='accessrecord_form'),
-    
-    # URL pattern for updating an existing access permission
-    path('<int:pk>/update/', AccessRecordsUpdateView.as_view(), name='accessrecord_form'),
-    
-    # URL pattern for deleting an access permission
-    path('<int:pk>/delete/', AccessRecordsDeleteView.as_view(), name='accessrecord_confirm_delete'),
-]
+    path('', AccessRecordList.as_view(), name='accessrecord_list'),
+    path('<int:RecordID>/', AccessRecordDetail.as_view(), name='accessrecord_detail'),
+    path('create/', AccessRecordCreate.as_view(), name='accessrecord_create'),
+    path('<int:RecordID>/update/', AccessRecordUpdate.as_view(), name='accessrecord_update'),
+    path('<int:RecordID>/delete/', AccessRecordDelete.as_view(), name='accessrecord_delete'),
