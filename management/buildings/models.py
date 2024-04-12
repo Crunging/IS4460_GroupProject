dd0f9af88ea293091ccb289ba370588e4be112ef
Arjun Pillai <arjunpillai35@gmail.com>
2024-04-11 21:28:46 -0600

    Commit

--- management/buildings/models.py
+++ management/buildings/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-
 class Building(models.Model):
     BuildingID = models.AutoField(primary_key=True)
     Building_Name = models.CharField(max_length=150)
     Year_Built = models.CharField(max_length=150)
-    Number_people = models.DateField(max_length=15)
-
+    Number_people = models.IntegerField()
