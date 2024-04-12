c1b6203019acf8141ba918fa24d42038b440a0d8
galaxter135 <144630286+galaxter135@users.noreply.github.com>
2024-04-11 23:06:56 -0600

    Update models.py

--- management/access_records/models.py
+++ management/access_records/models.py
@@ -6,7 +6,7 @@ from rooms.models import Room
 # Create your models here.
 class Access_Records(models.Model):
     RecordID = models.AutoField(primary_key=True)
-    UID = models.ForeignKey(Persons, on_delete.CASCADE)
+    UID = models.ForeignKey(Person, on_delete.CASCADE)
     BuildingID = models.ForeignKey(Building, on_delete=models.CASCADE)
     RoomID = models.ForeignKey(Room, on_delete=models.CASCADE)
     DateTime = models.DateTimeField()
