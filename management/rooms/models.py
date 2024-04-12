ab219ee69ab8053ccc14fffdf218eb41331c3fb3
marielisalv <156021731+marielisalv@users.noreply.github.com>
2024-04-11 20:50:31 -0600

    Update models.py

--- management/rooms/models.py
+++ management/rooms/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from buildings.models import Building
-class Room(models.Model):
-    RoomID = models.AutoField
-    BuildingID =models.ForeignKey(Building, on_delete=models.CASCADE)
-    Room_Number =models.IntegerField()
+from .models import Building 
+
+class Room(models.Model):  
+    RoomID = models.AutoField(primary_key=True) 
+    BuildingID = models.ForeignKey(Building, on_delete=models.CASCADE) 
+    Room_Number = models.IntegerField()
     Type = models.CharField(max_length=255)
 
-# Create your models here.
