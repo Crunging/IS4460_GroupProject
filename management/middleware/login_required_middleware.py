ae5975cae89dbc07692ca56606c5e9010c83606a
Crunging <loganpreston111@gmail.com>
2024-04-14 12:12:36 -0600

    Better login management + allows admin panel

--- management/middleware/login_required_middleware.py
+++ management/middleware/login_required_middleware.py
@@ -1,13 +1,21 @@
 from django.conf import settings
-from django.shortcuts import redirect
-from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.urls import resolve
 
 class LoginRequiredMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        # Exclude paths from requiring login (e.g., admin, login, logout)
+        if request.path.startswith('/admin/') or request.path.startswith('/login/') or request.path.startswith('/logout/'):
+            return None
+
+        # Apply login required except for the login and logout views
         if not request.user.is_authenticated:
-            if request.path not in [reverse('login'), reverse('logout'), '/admin/']:
-                return redirect(settings.LOGIN_URL)
-        return self.get_response(request)
+            return login_required(view_func)(request, *view_args, **view_kwargs)
+        return None
